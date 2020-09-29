d8e73b986c3e48abc52efc35e06293d7d5b3c979
Talita Passos <tata.ogp@gmail.com>
2020-09-29 12:49:39 -0300

    Adicionando livro e organizando descrições

--- src/components/CardItemCarrinho/CardItemCarrinho.jsx
+++ src/components/CardItemCarrinho/CardItemCarrinho.jsx
@@ -10,27 +10,26 @@ function CardItemCarrinho({ img, titulo, autor, preco, id, qtd }) {
     return (
         <div>
             <section className="card-livro_carrinho  grid-template-columns-4">
-                 <div>
-                     <img id="img-livro" src={img} alt={titulo}></img>
-                 </div>
-                 <section>
-                     <div>
-                         <h1 class="tituloMin">{titulo}</h1>
-                     </div>
-                     <div>
-                         <p>{autor}</p>
-                     </div>
-                 </section>
-                 <div className="quantidade">
-                     
-                     <button className="btnQtd" onClick={() => subQtdLivro(id)}><span>-</span></button>
-                     <input type="number" min="1" value={qtd} onChange={(val) => alteraQtdLivro(id, Number(val.target.value))} disabled></input>
-                     <button  className="btnQtd"  onClick={() => addQtdLivro(id)}><span>+</span></button>
-                 </div>
-                 <div>
-                    <h2>{formataValor(preco * qtd)}</h2>
-                 </div>
-             </section>
+                <div>
+                    <img id="img-livro" src={img} alt={titulo}></img>
+                </div>
+                <section>
+                    <div>
+                        <h1 class="tituloMin">{titulo}</h1>
+                    </div>
+                    <div>
+                        <p>{autor}</p>
+                    </div>
+                </section>
+                    <div className="quantidade">
+                        <button className="btnQtd" onClick={() => subQtdLivro(id)}><span>-</span></button>
+                        <input type="number" min="1" value={qtd} onChange={(val) => alteraQtdLivro(id, Number(val.target.value))} disabled></input>
+                        <button  className="btnQtd"  onClick={() => addQtdLivro(id)}><span>+</span></button>
+                    </div>
+                    <div>
+                        <h2>{formataValor(preco * qtd)}</h2>
+                    </div>
+            </section>
         </div>
     )
 }
--- src/pages/livroDescricao/LivroDescricao.jsx
+++ src/pages/livroDescricao/LivroDescricao.jsx
@@ -3,6 +3,7 @@ import "./style.css"
 import api from '../../services/api'
 import {useListaCompra} from '../../context/listaCompra';
 import { formataValor, intl } from '../../utils/intl'
+
 function LivroDescricao (props) {
     
     const {addLivroCarrinho} = useListaCompra();
@@ -10,25 +11,31 @@ function LivroDescricao (props) {
     const { match } = props
     const livroDetalhe = api.getLivros().filter(livro => livro.id == match.params.id)[0]
     return (   
-        <div className="livroDescricao">
+            <>
+            {livroDetalhe ?
+                <div className="livroDescricao">
              <div>
                      <img id="img-livroDetail" src={livroDetalhe.img} alt={livroDetalhe.titulo}></img>
                  </div>
-                 <div className="descricao">
+                 <div className="card-descricao">
                      <strong>{livroDetalhe.titulo}</strong>
                     <span>{livroDetalhe.autor}</span>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since
-                         the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
-                         but also the leap into electronic typesetting, remaining essentially unchanged.
-                         It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
-                         software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <p className='descricao'>{livroDetalhe.descricao}</p>
                  </div>
                  <div className="compra">
                      <h2>{formataValor(livroDetalhe.preco)}</h2>
                      <a className="botao-comprar" onClick={()=> addLivroCarrinho(livroDetalhe)}> Comprar</a>
                  </div>
+                 </div>
+    :
+                                
+                <div className="livroDescricao">
+               <h2 className="emptyDesc">Livro não cadastrado</h2> 
+                </div>
+}
+                 
           
-        </div>
+        </>
     );
 }
  
